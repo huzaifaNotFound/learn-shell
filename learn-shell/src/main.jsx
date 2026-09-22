fbf7aa32234e335ce63bb3bb8dc586f1fb9da9ba
huzaifa <idkmyname9009@gmail.com>
2026-09-22 10:58:31 +0530

    tab key fix

--- learn-shell/src/main.jsx
+++ learn-shell/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Tab') {
+    e.preventDefault();
+  }
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
